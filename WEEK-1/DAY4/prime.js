cbc25ae9400d2dc60e25d52e2eb804c08485ad10
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2025-10-15 23:43:02 +0530

    modifying prime.js file

--- WEEK-1/DAY4/prime.js
+++ WEEK-1/DAY4/prime.js
@@ -1,8 +1,18 @@
-if(a<=1){
-    console.log("not prime")
+var count =0
+var num=29
+if(num<=1){
+    console.log(`the number ${num} is INVALID`);
+    
+}
+for(var i=2;i<=Math.sqrt(num);i++){
+    if(num%i==0){
+        count ++
+        break
+    }
+}
+if(count==0){
+    console.log(`the number ${num} is PRIME`);
 }
 else{
-    for(let i=2;i<=Math.sqrt(a);i++){
-        
-    }
+    console.log(`the number ${num} is NOT-PRIME`);
 }
