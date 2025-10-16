45d3115845778b4d1f7791f1109be1fe08ba4a7f
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2025-10-16 10:19:49 +0530

    some little changes in week 2 and adding task1-print_alternates

---
+++ WEEK-2/DAY-4/task1-print_alternates.js
@@ -0,0 +1,10 @@
+const a=[10,20,30,40,50,60,70,80,90,100];
+for(let i=0;i<a.length;i=i+2){
+    console.log(a[i]);
+}
+
+console.log("this is another loop");
+
+for(let i=1;i<a.length;i=i+2){
+    console.log(a[i]);
+}
