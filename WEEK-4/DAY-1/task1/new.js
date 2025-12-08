f884b40631e71b24c17fa89b19574ae26d597424
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2025-12-08 16:05:01 +0530

    creating a design

---
+++ WEEK-4/DAY-1/task1/new.js
@@ -0,0 +1,9 @@
+
+const menuToggle = document.getElementById('menutoggle');
+const navlinks = document.getElementById('navlinks');
+
+menuToggle.addEventListener('click', function() {
+    navlinks.classList.toggle('active');
+});
+
+
