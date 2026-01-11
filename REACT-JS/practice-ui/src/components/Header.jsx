3a68e28889c69adb7c21296534fda5abd5c4e4d8
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-01-11 21:53:49 +0530

    almost the header is created

--- REACT-JS/practice-ui/src/components/Header.jsx
+++ REACT-JS/practice-ui/src/components/Header.jsx
@@ -3,9 +3,24 @@ import './Header.scss'
 
 function Header() {
   return (
-    <div>
-      <h1 className='hello'>hello guys</h1>
-    </div>
+    <header className="header">
+      <div className="name">
+        <h1>CineWave</h1>
+      </div>
+      <div className="description">
+        <ul className='ul'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Series</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">Discover</a></li>
+          <li><a href="#">Watch later</a></li>
+        </ul>
+      </div>
+      <div className="search">
+        <button>🔎</button>
+        <img src="" alt="logo" />
+      </div>
+    </header>
   )
 }
 
