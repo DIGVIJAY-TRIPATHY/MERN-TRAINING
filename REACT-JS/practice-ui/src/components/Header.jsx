3e2ff80f8c7a8044a69a7c168ac70a2c73e6334d
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-01-11 21:01:24 +0530

    just creating a webpage for ui practice

---
+++ REACT-JS/practice-ui/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.scss'
+
+function Header() {
+  return (
+    <div>
+      <h1 className='hello'>hello guys</h1>
+    </div>
+  )
+}
+
+export default Header
