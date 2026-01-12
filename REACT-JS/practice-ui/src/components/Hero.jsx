098978da6529964a85e91949b290c66bc15a514c
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-01-12 19:34:42 +0530

    almost created the hero

--- REACT-JS/practice-ui/src/components/Hero.jsx
+++ REACT-JS/practice-ui/src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Hero.scss'
+import im3 from '../assets/im-3.png'
+import im1 from '../assets/im-1.png'
+import im2 from '../assets/im-2.png'
 
 function Hero() {
   return (
@@ -11,7 +14,7 @@ function Hero() {
             <h1>CineWave</h1>
             <div className="buttons">
                 <button className="btn watch">Watch Now</button>
-                <button className="btn info">More Info</button>
+                <button className="btn m-info">More Info</button>
             </div>
         </div>
         <div className="info">
@@ -26,6 +29,32 @@ function Hero() {
                 <button className="btn-g">Horror</button>
                 <button className="btn-g">Romance</button>
                 <button className="btn-g">Sci-Fi</button>
+            </div>
+        </div>
+        <div className="cards">
+            <div className="card">
+                <img src={im3} alt="hi" />
+                <h4>movie name</h4>
+                <div className='des-btn'>
+                    <p>movie description</p>
+                    <button className='card-watch'>Watch Now</button>
+                </div>
+            </div>
+            <div className="card">
+                <img src={im1} alt="" />
+                <h4>movie name</h4>
+                <div className='des-btn'>
+                    <p>movie description</p>
+                    <button className='card-watch'>Watch Now</button>
+                </div>
+            </div>
+            <div className="card">
+                <img src={im2} alt="" />
+                <h4>movie name</h4>
+                <div className='des-btn'>
+                    <p>movie description</p>
+                    <button className='card-watch'>Watch Now</button>
+                </div>
             </div>
         </div>
     </section>
